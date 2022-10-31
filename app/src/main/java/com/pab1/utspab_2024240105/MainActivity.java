25b38a6020ca53e087e0f4a5fb1187ee03ffbc23
azihakim <azi.hakim@mhs.mdp.ac.id>
2022-10-31 14:11:22 +0700

    new

---
+++ app/src/main/java/com/pab1/utspab_2024240105/MainActivity.java
@@ -0,0 +1,14 @@
+package com.pab1.utspab_2024240105;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
